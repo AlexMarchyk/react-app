bb6364b7d70af31f03c9a8bc069209a427899dba
AlexMarchyk <alexmarcyk@gmail.com>
2021-10-11 21:35:32 +0300

    add delete Item function

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,7 +2,7 @@ import React from "react";
 import PostListItem from '../post-list-item';
 import './post-list.css'
 
-const PostList = ( {posts}) =>{
+const PostList = ( {posts,onDelete}) =>{
 
     const elements = posts.map((item)=>{
         // restr object
@@ -11,7 +11,8 @@ const PostList = ( {posts}) =>{
             <li key={id} className='list-group-item'>
                 <PostListItem 
                     label={itemProps.label}
-                    important ={itemProps.important} /> 
+                    important ={itemProps.important}
+                    onDelete ={()=>onDelete(id)} /> 
                     {/* or spread  
                         <PostListItem{...item}  /> 
                     */}
