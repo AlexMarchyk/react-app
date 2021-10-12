f0da3380f8e9baec63093b3a27e79ddcba2242b0
AlexMarchyk <alexmarcyk@gmail.com>
2021-10-12 20:49:13 +0300

    add filter, fix bags with likes, add work of inputs

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,11 +2,11 @@ import React from "react";
 import './app-header.css';
 
 
-const AppHeader =()=>{
+const AppHeader =({liked,allPosts})=>{
     return (
         <div className="app-header d-flex">
             <h1>Alex Marchyk</h1>
-            <h2> 5 posts, like 0 </h2>
+            <h2>{allPosts}  posts, liked {liked} </h2>
         </div>
 
     )
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,7 +2,7 @@ import React from "react";
 import PostListItem from '../post-list-item';
 import './post-list.css'
 
-const PostList = ( {posts,onDelete}) =>{
+const PostList = ( {posts, onDelete, onToggleImportant, onToggleLiked}) =>{
 
     const elements = posts.map((item)=>{
         // restr object
@@ -10,9 +10,11 @@ const PostList = ( {posts,onDelete}) =>{
         return(
             <li key={id} className='list-group-item'>
                 <PostListItem 
-                    label={itemProps.label}
-                    important ={itemProps.important}
-                    onDelete ={()=>onDelete(id)} /> 
+                    
+                    {...itemProps}
+                    onDelete ={()=>onDelete(id)}
+                    onToggleImportant={()=>onToggleImportant(id)}
+                    onToggleLiked={()=>onToggleLiked(id)} /> 
                     {/* or spread  
                         <PostListItem{...item}  /> 
                     */}
